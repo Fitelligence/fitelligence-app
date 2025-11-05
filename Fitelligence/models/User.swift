bf9990d22c1252d115a05394332c0dffed3b6b21
Austin <austin@Austins-MacBook-Air.local>
2025-11-04 23:07:34 -0500

    add data model for User

---
+++ Fitelligence/models/User.swift
@@ -0,0 +1,23 @@
+//
+//  User.swift
+//  Fitelligence
+//
+//  Created by Austin on 11/4/25.
+//
+
+import ParseSwift
+import Foundation
+
+struct User: ParseUser {
+    var objectId: String?
+    var createdAt: Date?
+    var updatedAt: Date?
+    var ACL: ParseACL?
+    var originalData: Data?
+    
+    var username: String?
+    var email: String?
+    var emailVerified: Bool?
+    var password: String?
+    var authData: [String: [String: String]?]?
+}
