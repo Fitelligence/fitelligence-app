fc57fa564b0f17e18b614b879134e19aaf1c0359
Austin <austin@Austins-MacBook-Air.local>
2025-11-20 02:24:30 -0500

    initialize parse swift configuration

--- Fitelligence/FitelligenceApp.swift
+++ Fitelligence/FitelligenceApp.swift
@@ -6,9 +6,18 @@
 //
 
 import SwiftUI
+import ParseSwift
 
 @main
 struct FitelligenceApp: App {
+    init() {
+        ParseSwift.initialize(
+            applicationId: "1g9mCtQTndUdapOL2URSjzBarbEVJpBxXI09peRa",
+            clientKey: "Uc7vWW8kRkgrEgm0TDQ9l9CkrST9rQUvkZQluN5H",
+            serverURL: URL(string: "https://parseapi.back4app.com")!
+        )
+    }
+    
     var body: some Scene {
         WindowGroup {
             LoginView()
